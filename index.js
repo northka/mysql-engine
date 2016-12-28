ec4b7bb48c6bded71a3ddcde3b21d08d95c2fe75
DESKTOP-S0OAMUC\northka.chen <northka.chen@gmail.com>
2016-12-28 14:08:34 +0800

    暴露出request和parseReqObj 的api

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 /**
  * Created by northka.chen on 2016/12/27.
  */
+const request     = require('./lib/request')
+const parseReqObj = require('./lib/parseReqObj')
+module.exports = {
+    request,
+    parseReqObj
+}
